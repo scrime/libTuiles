e54d28da0e8983360d909e6c0ceeb0a8316e07d6
Florent Berthaut <florent@hitmuri.net>
2013-07-10 15:40:26 +0200

    Added template of MonitorTuile

---
+++ src/MonitorTuile.cpp
@@ -0,0 +1,82 @@
+/***************************************************************************
+ *  SwitchTuile.cpp
+ *  2012- Florent Berthaut
+ *  ANR INEDIT Project
+ *  This file is part of libTuiles
+ ****************************************************************************/
+
+#include "SwitchTuile.hpp"
+
+using namespace std;
+
+namespace tuiles {
+
+SwitchTuile::SwitchTuile(): OpTuile(), m_selectedChild(NULL) {}
+
+SwitchTuile::~SwitchTuile() {}
+
+void SwitchTuile::updateWindows() {
+/*
+    if(m_selectedChild<m_children.size()) {
+        m_length=m_children[m_selectedChild]->getLength();
+        m_syncWindowSize=m_children[m_selectedChild]->getSyncWindowSize();
+        m_leftOffset=m_children[m_selectedChild]->getLeftOffset();
+        m_rightOffset=m_children[m_selectedChild]->getRightOffset();
+        if(m_parent) {
+            m_parent->updateWindows();
+        }
+        updateProperties();
+    }
+*/
+}
+
+void SwitchTuile::addChild(Tuile* newChild) {
+    m_children.push_back(newChild);
+    selectChild(m_selectedChild);
+    updateWindows();
+
+    //TODO add proc children 
+}
+
+void SwitchTuile::selectChild(const unsigned int& childID) {
+/*
+    if(childID<m_children.size()) {
+        m_selectedChild=child;
+        for(unsigned int c=0;c<m_children.size();++c) {
+            if(c==m_selectedChild) {
+                m_children[c]->unmute();
+            }
+            else {
+                m_children[c]->mute();
+            }
+        }
+        updateWindows();
+    }
+*/
+
+//TODO send SetSelectedChild command
+
+}
+
+void SwitchTuile::selectChild(Tuile* child) {
+
+
+}
+
+void SwitchTuile::processPos(const float& pos, const Voice& voice) {
+    Tuile::processPos(pos, voice);
+    if(!m_muted) {
+        //process only the activated child
+        m_selectedChild->processPos(m_position, voice);
+    }
+}
+
+float SwitchTuile::getChildPositionAtPos(const unsigned int& child, 
+                                                const float& pos) {
+    //FIXME 
+    //return pos-(m_procLeftOffset-m_procChildren[child]->m_procLeftOffset);
+    return pos;
+}
+
+}
+
---
+++ tuiles/Observer.hpp
@@ -0,0 +1,39 @@
+/***************************************************************************
+ *  Command.hpp
+ *  2012- Florent Berthaut
+ *  ANR INEDIT Project
+ *  This file is part of libTuiles
+ ****************************************************************************/
+
+#ifndef Command_h
+#define Command_h
+
+#include <vector>
+#include <list>
+#include <string>
+
+namespace tuiles {
+
+class Command {	
+	public:
+		Command();
+		virtual ~Command();
+
+		virtual void run()=0;
+		void clean();
+
+        virtual void createClones(const unsigned int& nbClones)=0;
+        inline void setModel(Command* com){m_model=com;}
+        Command* popClone();
+
+	protected:
+        std::string m_name;
+        std::list<Command*> m_clones;
+        Command* m_model;
+        void pushClone(Command*);
+};
+
+}
+
+#endif
+
